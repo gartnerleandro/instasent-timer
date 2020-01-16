02dc2102f71fba55013f94d36e82f96e6a24ebd0
gartnerleandro <gartnerleandro@gmail.com>
2020-01-16 23:56:15 +0100

    Feature: create app core

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+
+import TimerButtons from './components/timerButtons';
+import Separator from './components/separator';
+
+import formatTime from './utils/formatter';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isTimerWorking: false,
+      isTimePaused: false,
+      startTime: 0,
+      currentTime: 0,
+    };
+  }
+
+  updateTimer = () => this.setState((prevState) => ({
+    currentTime: Date.now() - prevState.startTime,
+  }));
+
+  onStopPress = () => {
+    clearInterval(this.timer);
+    this.setState({ isTimePaused: true });
+  }
+
+  onStartPress = () => {
+    this.setState((prevState) => ({
+      startTime: Date.now() - prevState.currentTime,
+      isTimerWorking: true,
+      isTimePaused: false,
+    }));
+
+    this.timer = setInterval(this.updateTimer, 10);
+  };
+
+  onResetPress = () => {
+    clearInterval(this.timer);
+    this.setState({
+      startTime: 0,
+      currentTime: 0,
+      isTimerWorking: false,
+      isTimePaused: false,
+    });
+  };
+  
+  render() {
+    const { currentTime, isTimePaused, isTimerWorking } = this.state;
+
+    const seconds = formatTime(Math.floor(currentTime / 1000) % 60);
+    const minutes = formatTime(Math.floor(currentTime / 60000) % 60);
+    const hours = formatTime(Math.floor(currentTime / 3600000));
+
+    return (
+      <div className="app-container">
+        <div className="timer-display">
+          <span className="timer-value">
+            {hours}
+          </span>
+          <Separator />
+          <span className="timer-value">
+            {minutes}
+          </span>
+          <Separator />
+          <span className="timer-value">
+            {seconds}
+          </span>
+        </div>
+        <TimerButtons
+          isTimePaused={isTimePaused}
+          isTimerWorking={isTimerWorking}
+          onStartPress={this.onStartPress}
+          onStopPress={this.onStopPress}
+          onResetPress={this.onResetPress}
+        />
+      </div>
+    );
+  }
+}
+
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,91 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { mount } from 'enzyme';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const startButton = 'button.button-style.start';
+const stopButton = 'button.button-style.stop';
+const resetButton = 'button.button-style.reset';
+
+describe('Timer App', () => {
+  it('render buttons depending of timer state', () => {
+    const wrapper = mount(<App />);
+  
+    const startButtonElement = wrapper.find(startButton);
+    expect(startButtonElement).toHaveLength(1);
+
+    wrapper.unmount();
+  });
+
+  it('display timer', () => {
+    const wrapper = mount(<App />);
+
+    const timerDisplayElement = wrapper.find('.timer-display');
+
+    expect(timerDisplayElement).toHaveLength(1);
+
+    wrapper.unmount();
+  });
+
+  it('render buttons depending of timer state', () => {
+    const wrapper = mount(<App />);
+  
+    expect(wrapper.find(startButton)).toHaveLength(1);
+    expect(wrapper.find(stopButton)).toHaveLength(0);
+    expect(wrapper.find(resetButton)).toHaveLength(0);
+
+    wrapper.find(startButton).simulate('click');
+
+    expect(wrapper.find(startButton)).toHaveLength(0);
+    expect(wrapper.find(stopButton)).toHaveLength(1);
+    expect(wrapper.find(resetButton)).toHaveLength(0)
+
+    wrapper.find(stopButton).simulate('click');
+
+    expect(wrapper.find(startButton)).toHaveLength(1);
+    expect(wrapper.find(stopButton)).toHaveLength(0);
+    expect(wrapper.find(resetButton)).toHaveLength(1);
+
+    wrapper.find(resetButton).simulate('click');
+
+    expect(wrapper.find(startButton)).toHaveLength(1);
+    expect(wrapper.find(stopButton)).toHaveLength(0);
+    expect(wrapper.find(resetButton)).toHaveLength(0);
+
+    wrapper.unmount();
+  });
+
+  it('reset timer on click reset button', () => {
+    const wrapper = mount(<App />);
+    jest.useFakeTimers()
+
+    expect(wrapper.state().isTimerWorking).toBe(false);
+    expect(wrapper.state().isTimePaused).toBe(false);
+
+    const startButtonElement = wrapper.find(startButton);
+    startButtonElement.simulate('click');
+
+    jest.advanceTimersByTime(1000);
+
+    expect(wrapper.state().isTimerWorking).toBe(true);
+    expect(wrapper.state().isTimePaused).toBe(false);
+    expect(wrapper.state().currentTime).toBeGreaterThan(0);
+
+    jest.advanceTimersByTime(1000);
+
+    const stopButtonElement = wrapper.find(stopButton);
+    stopButtonElement.simulate('click');
+
+    expect(wrapper.state().isTimerWorking).toBe(true);
+    expect(wrapper.state().isTimePaused).toBe(true);
+    expect(wrapper.state().currentTime).toBeGreaterThan(1);
+
+    const resetButtonElement = wrapper.find(resetButton);
+    resetButtonElement.simulate('click');
+
+    expect(wrapper.state().isTimerWorking).toBe(false);
+    expect(wrapper.state().isTimePaused).toBe(false);
+    expect(wrapper.state().currentTime).toBe(0);
+    wrapper.unmount();
+  });
 });
